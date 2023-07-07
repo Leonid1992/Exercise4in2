d026c53b9d450ee3cb6d8f1da52f0f68d5b8b830
Leonid <leonid.vilcu@vlc2.com>
2023-07-07 14:45:24 +0200

    dfsdfsg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+// someone enters “ alphabet “ (notice the spaces) as input into a method, make that
+      //  method output “alphabeT” instead
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String input = "Alphabet";
+       System.out.println("Prima: " + input);
+
+       String stripInput = input.strip();
+       String nrInput = stripInput.substring(0,stripInput.length()-1);
+       System.out.println("Durante: " + nrInput);
+
+       Character cambioLett = stripInput.charAt(stripInput.length()-1);
+       System.out.println("Fine: " + nrInput + cambioLett.toString().toUpperCase());
     }
 }
